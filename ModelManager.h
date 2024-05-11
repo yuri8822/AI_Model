ecaab1c889d983d0136422b8cc9ca1db2a23fa59
Umar.exe <61727014+yuri8822@users.noreply.github.com>
2024-05-11 19:54:30 +0500

    Add save model functionality to ModelManager class

--- ModelManager.h
+++ ModelManager.h
@@ -44,6 +44,48 @@ public:
     void SaveModel()
     {
         cout << "Saving model\n";
+
+        ofstream Save("Model.dat");
+
+        // Save input layer:
+        for (int i = 0; i < model.inputLayer.size(); i++)
+        {
+            Save << model.inputLayer[i].activation << "\n"
+                 << model.inputLayer[i].bias << "\n";
+        }
+        // Save hidden layer 1:
+        for (int i = 0; i < model.hiddenLayer1.size(); i++)
+        {
+            Save << model.hiddenLayer1[i].activation << "\n"
+                 << model.hiddenLayer1[i].bias << "\n";
+
+            for (int j = 0; j < model.hiddenLayer1[i].weights.size(); j++)
+            {
+                Save << model.hiddenLayer1[i].weights[j] << "\n";
+            }
+        }
+        // Save hidden layer 2:
+        for (int i = 0; i < model.hiddenLayer2.size(); i++)
+        {
+            Save << model.hiddenLayer2[i].activation << "\n"
+                 << model.hiddenLayer2[i].bias << "\n";
+
+            for (int j = 0; j < model.hiddenLayer2[i].weights.size(); j++)
+            {
+                Save << model.hiddenLayer2[i].weights[j] << "\n";
+            }
+        }
+        // Save output layer:
+        for (int i = 0; i < model.outputLayer.size(); i++)
+        {
+            Save << model.outputLayer[i].activation << "\n"
+                 << model.outputLayer[i].bias << "\n";
+
+            for (int j = 0; j < model.outputLayer[i].weights.size(); j++)
+            {
+                Save << model.outputLayer[i].weights[j] << "\n";
+            }
+        }
     }
     void LoadModel()
     {
--- main.cpp
+++ main.cpp
@@ -31,4 +31,6 @@ int main()
     
     ModelManager MM;
 
+    MM.SaveModel();
+    // MM.LoadModel();
 }
